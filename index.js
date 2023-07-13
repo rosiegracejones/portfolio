5be91fdf431c3aad401f6e9a2e6bb12b1928ab58
Rosie Jones <rosiegracejones@gmail.com>
2023-07-13 22:29:51 +0100

    transitions & links updated

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var colors = ['--orange', '--violet', '--lilac', '--palelilac', '--green', '--yellow', '--white', '--navy'];
+// var colors = ['--orange', '--violet', '--lilac', '--palelilac', '--green', '--yellow', '--white', '--navy'];
 
-function changeColours () {
-    var root = document.documentElement;
-    var randomColor = colors[Math.floor(Math.random() * colors.length)];
-    root.style.setProperty('--color', 'var(' + randomColor + ')');
-}
-
+// function changeColours () {
+//     var root = document.documentElement;
+//     var randomColor = colors[Math.floor(Math.random() * colors.length)];
+//     root.style.setProperty('--color', 'var(' + randomColor + ')');
+// }
