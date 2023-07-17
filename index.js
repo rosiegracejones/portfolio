0f8dacc86cafd7e120a28def152b07a142974a4c
Rosie Jones <rosiegracejones@gmail.com>
2023-07-17 22:38:46 +0100

    some style/colour changes

--- index.js
+++ index.js
@@ -5,3 +5,10 @@
 //     var randomColor = colors[Math.floor(Math.random() * colors.length)];
 //     root.style.setProperty('--color', 'var(' + randomColor + ')');
 // }
+
+function toggleColour () {
+    document.querySelector('body').style.backgroundColor = 'var(--grey)';
+    document.querySelector('.navbar').style.backgroundColor = 'var(--grey)';
+    document.querySelector('p').style.color = 'var(--white)';
+    document.querySelector('a').style.color = 'var(--white)';
+}
